87065e470f5c08ebde5eb55dbb927c765cd8825f
D-Berg <danielfjberg@gmail.com>
2024-08-29 10:56:49 +0200

    lexer tests 0 to 3 succeeds

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,11 +1,16 @@
 const std = @import("std");
-const Token = @import("token.zig").Token;
-const Allocator = std.mem.Allocator;
+const token_mod = @import("token.zig");
 
+const Allocator = std.mem.Allocator;
+const Token = token_mod.Token;
+const Keywords = token_mod.Keywords;
 
 const print = std.debug.print;
+const log = std.log;
 const expect = std.testing.expect;
 const expectEqualSlices = std.testing.expectEqualSlices;
+
+
 
 const TokenError = error {
     unsupported_char,
@@ -41,57 +46,168 @@ pub const Lexer = struct {
         l.readPosition += 1;
     }
 
-    pub fn NextToken(l: *Lexer, allocator: Allocator, ) !Token {
+    pub fn NextToken(l: *Lexer, allocator: Allocator, key_words: *const Keywords) !Token {
 
-        const maybe_tok: TokenError!Token = switch (l.ch) {
-            '=' => try Token.new(allocator, Token.Kind.Assign, l.ch),
-            ';' => try Token.new(allocator, Token.Kind.Semicolon, l.ch),
-            '(' => try Token.new(allocator, Token.Kind.Lparen, l.ch),
-            ')' => try Token.new(allocator, Token.Kind.Rparen, l.ch),
-            '{' => try Token.new(allocator, Token.Kind.Lbrace, l.ch),
-            '}' => try Token.new(allocator, Token.Kind.Rbrace, l.ch),
-            '+' => try Token.new(allocator, Token.Kind.Plus, l.ch),
-            ',' => try Token.new(allocator, Token.Kind.Comma, l.ch),
-            0 => Token { .kind = Token.Kind.Eof, .literal = "" },
-            else => TokenError.unsupported_char,
+        var read_next_char = true; 
 
-        };
+        defer {
+            if (read_next_char) l.readChar();
+        }
 
-        l.readChar();
-        return maybe_tok;
+        l.skipWhiteSpace();
 
+        const chars = [1]u8{ l.ch };
+        switch (l.ch) {
+            '=' => { return try Token.init(allocator, Token.Kind.Assign, &chars); },
+            ';' => { return try Token.init(allocator, Token.Kind.Semicolon, &chars); },
+            '(' => { return try Token.init(allocator, Token.Kind.Lparen, &chars); },
+            ')' => { return try Token.init(allocator, Token.Kind.Rparen, &chars); },
+            '{' => { return try Token.init(allocator, Token.Kind.Lbrace, &chars); },
+            '}' => { return try Token.init(allocator, Token.Kind.Rbrace, &chars); },
+            '+' => { return try Token.init(allocator, Token.Kind.Plus, &chars); },
+            ',' => { return try Token.init(allocator, Token.Kind.Comma, &chars); },
+            0 => { return try Token.init(allocator, Token.Kind.Eof, ""); },
+            else => {
+                if (isLetter(l.ch)) {
+
+                    read_next_char = false;
+                    const start = l.position;
+                    l.readIdentifier();
+                    const end = l.position;
+
+                    const identifer_str = l.input[start..end];
+                    const token_kind = key_words.words.get(identifer_str) orelse Token.Kind.Ident;
+
+                    return try Token.init(allocator, token_kind, identifer_str);
+
+                } if (isDigit(l.ch)) {
+                    const start = l.position;
+                    l.readNumber();
+                    const end = l.position;
+
+                    const number_str = l.input[start..end];
+                    return try Token.init(allocator, Token.Kind.Int, number_str);
+
+                } else {
+                    return try Token.init(allocator, Token.Kind.Illegal, &chars);
+                }
+            }
+        }
+
+    }
+
+    fn readIdentifier(l: *Lexer) void {
+        while (isLetter(l.ch)) l.readChar();
+    }
+
+    fn readNumber(l: *Lexer) void {
+        while (isDigit(l.ch)) l.readChar();
+
+    }
+
+    fn skipWhiteSpace(l: *Lexer) void {
+        while (l.ch == ' ' or l.ch == '\t' or l.ch == '\n' or l.ch == '\r') {
+            l.readChar();
+        }
     }
 };
 
+fn isLetter(ch: u8) bool {
+
+    const is_letter = ('a' <= ch and ch <= 'z') or 
+        ('A' <= ch and ch <= 'Z') or ch == '_';
+
+    return is_letter;
+}
+
+fn isDigit(ch: u8) bool {
+    return '0' <= ch and ch <= '9';
+}
+
 test "next token" {
+    const allocator = std.testing.allocator;
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    var key_words = try Keywords.init(allocator);
+    defer key_words.deinit();
 
-    const allocator = gpa.allocator();
-
-    const input = "=+(){},;";
+    const input = 
+        \\let five = 5;
+        \\let ten = 10;
+        \\
+        \\let add = fn(x, y) {
+        \\  x + y;
+        \\};
+        \\
+        \\let result = add(five, ten);
+    ;
 
     var l = Lexer.new(input);
 
     const token_tests = [_]Token {
-        Token { .kind = Token.Kind.Assign, .literal = "="},
-        Token { .kind = Token.Kind.Plus, .literal = "+"},
+        try Token.init(allocator, Token.Kind.Let, "let"),
+        try Token.init(allocator, Token.Kind.Ident, "five"),
+        try Token.init(allocator, Token.Kind.Assign, "="),
+        try Token.init(allocator, Token.Kind.Int, "5"),
+        try Token.init(allocator, Token.Kind.Semicolon, ";"),
+        try Token.init(allocator, Token.Kind.Let, "let"),
+        try Token.init(allocator, Token.Kind.Ident, "ten"),
+        try Token.init(allocator, Token.Kind.Assign, "="),
+        try Token.init(allocator, Token.Kind.Int, "10"),
+        try Token.init(allocator, Token.Kind.Semicolon, ";"),
+        try Token.init(allocator, Token.Kind.Let, "let"),
+        try Token.init(allocator, Token.Kind.Ident, "add"),
+        try Token.init(allocator, Token.Kind.Assign, "="),
+        try Token.init(allocator, Token.Kind.Function, "fn"),
+        try Token.init(allocator, Token.Kind.Lparen, "("),
+        try Token.init(allocator, Token.Kind.Ident, "x"),
+        try Token.init(allocator, Token.Kind.Comma, ","),
+        try Token.init(allocator, Token.Kind.Ident, "y"),
+        try Token.init(allocator, Token.Kind.Rparen, ")"),
+        try Token.init(allocator, Token.Kind.Lbrace, "{"),
+        try Token.init(allocator, Token.Kind.Ident, "x"),
+        try Token.init(allocator, Token.Kind.Plus, "+"),
+        try Token.init(allocator, Token.Kind.Ident, "y"),
+        try Token.init(allocator, Token.Kind.Semicolon, ";"),
+        try Token.init(allocator, Token.Kind.Rbrace, "}"),
+        try Token.init(allocator, Token.Kind.Semicolon, ";"),
+        try Token.init(allocator, Token.Kind.Let, "let"),
+        try Token.init(allocator, Token.Kind.Ident, "result"),
+        try Token.init(allocator, Token.Kind.Assign, "="),
+        try Token.init(allocator, Token.Kind.Ident, "add"),
+        try Token.init(allocator, Token.Kind.Lparen, "("),
+        try Token.init(allocator, Token.Kind.Ident, "five"),
+        try Token.init(allocator, Token.Kind.Comma, ","),
+        try Token.init(allocator, Token.Kind.Ident, "ten"),
+        try Token.init(allocator, Token.Kind.Rparen, ")"),
+        try Token.init(allocator, Token.Kind.Semicolon, ";"),
+        try Token.init(allocator, Token.Kind.Eof, ""),
     };
+
+    defer {
+        for (token_tests) |token| {
+            token.deinit();
+        }
+    }
 
     for (token_tests, 0..) |expected_token, i| {
 
-        const tok = try l.NextToken(allocator);
+        const tok = l.NextToken(allocator, &key_words) catch |err| {
+            print("token test {}: Failed with Error: {}.\n", .{
+                i, err
+            });
+            return err;
+        };
+
         defer allocator.free(tok.literal);
 
         expect(tok.kind == expected_token.kind) catch |err| {
 
-            print("token_test[{}] - tokenKind wrong. expected={}, got={}.\n", .{
+            print("token test {}: Failed, tokenKind wrong. expected={}, got={}.\n", .{
                 i, expected_token.kind, tok.kind
             });
 
-            print("l.char = {}, l.position = {}, l.readPosition = {}\n", .{
-                l.ch, l.position, l.readPosition
+            print("Token: kind = {}, .literal = '{s}'\n", .{
+                tok.kind, tok.literal
             });
             print("Error: {}", .{err});
             return err;
@@ -99,7 +215,7 @@ test "next token" {
 
         expectEqualSlices(u8, expected_token.literal, tok.literal) catch |err| {
 
-            print("token_tests[{}]-literal wrong, expected='{s}', got='{s}'.\n", .{
+            print("token tests {}: Failed, literal wrong, expected='{s}', got='{s}'.\n", .{
                 i, expected_token.literal, tok.literal
 
             });
@@ -107,7 +223,12 @@ test "next token" {
             return err;
 
         };
-        print("literal: {s}\n", .{tok.literal});
+
+        print("\u{001b}[31mtoken test {}: succeded\u{001b}[0m, kind={}, literal='{s}'\n", .{
+            i, tok.kind,  tok.literal
+        });
+
+
 
     }
 }
--- src/token.zig
+++ src/token.zig
@@ -2,18 +2,23 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const Token = struct {
+    allocator: Allocator,
     kind: Kind,
-    literal: []const u8,
+    literal: []const u8, 
+    
+    pub fn init(allocator: Allocator, kind: Token.Kind, chars: []const u8) !Token {
 
-    pub fn new(allocator: Allocator, kind: Token.Kind, ch: u8) !Token {
-
-        const str = try allocator.alloc(u8, 1);
-        str[0] = ch;
+        const str = try allocator.dupe(u8, chars);
 
         return .{
+            .allocator = allocator,
             .kind = kind, 
             .literal = str
         };
+    }
+
+    pub fn deinit(t: *const Token) void {
+        t.allocator.free(t.literal);
     }
 
     pub const Kind = enum {
@@ -37,4 +42,32 @@ pub const Token = struct {
         Function,
         Let,
     };
+};
+
+
+pub const Keywords = struct {
+    
+    words: std.StringHashMap(Token.Kind),
+
+    pub fn init(allocator: Allocator) !Keywords {
+
+        var words = std.StringHashMap(Token.Kind).init(allocator);
+        try words.put("let", Token.Kind.Let);
+        try words.put("fn", Token.Kind.Function);
+
+        return .{
+            .words = words,
+        };
+        
+    }
+
+    pub fn deinit(keywords: *Keywords) void {
+
+        keywords.words.deinit();
+
+    }
+
+
+
+    
 };
