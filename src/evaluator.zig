34dff2b97b5b3b64f45121092ae76062a04000f2
D-Berg <danielfjberg@gmail.com>
2024-09-28 16:12:36 +0200

    added todo

--- src/evaluator.zig
+++ src/evaluator.zig
@@ -1010,3 +1010,13 @@ test "eval counter p.150" {
     }
 
 }
+
+
+// TODO add tests for 
+//    >> let add = fn(a, b) { a + b };
+//    >> let sub = fn(a, b) { a - b };
+//    >> let applyFunc = fn(a, b, func) { func(a, b) };
+//    >> applyFunc(2, 2, add);
+//    4
+//    >> applyFunc(10, 2, sub);
+//    8
