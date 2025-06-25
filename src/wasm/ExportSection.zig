d8f78e6e4632d8264149ecd84db2abbf6e7c7a5e
D-Berg <danielfjberg@gmail.com>
2025-06-25 13:27:46 +0200

    parsing of export section

--- src/wasm/ExportSection.zig
+++ src/wasm/ExportSection.zig
@@ -9,6 +9,7 @@ const StringHashMap = std.StringHashMapUnmanaged;
 
 const Self = @This();
 
+/// key mem is not allocated
 exports: StringHashMap(wasm.ExportDescription),
 
 pub const empty = Self{ .exports = .empty };
@@ -61,7 +62,31 @@ pub fn section(self: *Self) Section {
 
 pub fn parse(ctx: *anyopaque, gpa: Allocator, bytes: []const u8) !void {
     const self: *Self = @ptrCast(@alignCast(ctx));
-    _ = self;
-    _ = gpa;
     log.debug("decoding len = {}, {x}", .{ bytes.len, bytes });
+
+    var u32_converter = wasm.LEB128Encoder(u32).init;
+
+    const n_exports, var enc_len = try u32_converter.decode(bytes);
+    var at = enc_len;
+
+    for (0..n_exports) |_| {
+        const name_len, enc_len = try u32_converter.decode(bytes[at..]);
+        at += enc_len;
+
+        const name = bytes[at..(at + name_len)];
+        log.debug("found export {s}", .{name});
+        at += name_len;
+
+        const kind: wasm.ExportDescription.Kind = @enumFromInt(bytes[at]);
+        log.debug("kind = {s}", .{@tagName(kind)});
+        at += 1;
+
+        const idx, enc_len = try u32_converter.decode(bytes[at..]);
+        at += enc_len;
+
+        log.debug("idx = {}", .{idx});
+
+        // NOTE: name is only valid as long as bytes are (wasm input program)
+        try self.exports.put(gpa, name, .{ .idx = idx, .kind = kind });
+    }
 }
